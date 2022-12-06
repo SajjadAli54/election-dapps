b731a0d490b6bfb3f519d3a95fb335b44cb05dbf
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2022-12-06 23:43:56 +0500

    Added some other tests

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -11,6 +11,8 @@ contract Election {
     }
 
     mapping(uint => Candidate) public candidates;
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
 
     uint public candidatesCount;
 
@@ -25,6 +27,20 @@ contract Election {
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
     }
 
 
